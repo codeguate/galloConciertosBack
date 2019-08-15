e26b587152120762fdf4cc87569a9c8b4c4230f1
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-08-15 16:10:50 -0600

    [UPDATED] aded responsive to email template

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,10 +1,18 @@
-<div style="width: 1000px;height: 500px;display: flex;background-image: url(http://test.codeguate.com/gallo/fondo-canciones.png);background-size: cover">
-  <div style="width: 700px;height: 400px;margin-left: 150px;text-align: center;background-color: #922c2a99;margin-top: 50px;">
-      <img src="http://test.codeguate.com/gallo/logo.png" style="height: 200px;width: auto;margin-top: 25px">
-      <h2>Nombre: <span id="name-email">{{ $username }}</span></h2>
-      <h3>contraseña: <span id="password-email">********</span></h3>
-      <p style="text-align:center;width:100%;">
+<html>
+  <head>
+
+  </head>
+  <body style="margin: 0px;">
+    <div style="margin: auto;width: 100vw;height: auto;display: flex;background-image: url(http://test.codeguate.com/gallo/fondo-canciones.png);background-size: 100% 100%;">
+      <div style="width: 100vw;height: 100vh;text-align: center;background-color: #922c2a99;margin-top: 0px;color: #ffffff;">
+        <img src="http://test.codeguate.com/gallo/logo.png" style="height: 30vh;width: auto;margin-top: 25px;">
+        <h2 style="font-size: 4vh;">Nombre:<span id="name-email"> {{ $username }} </span></h2>
+      <h3 style="font-size: 4vh;">Contrase&ntilde;a: <span id="password-email">*****</span></h3>
+      <p style="text-align:center;width:100%;font-size: 2vh;">
           Ahora puedes votar por las canciones que deseas que suenen en el concierto de independencia GALLO.
         </p>
   </div>
 </div>
+
+</body>
+</html>
