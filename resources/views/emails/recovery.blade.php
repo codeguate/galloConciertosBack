f1a3b7d0cd7b119b4f7cf7a9e38bda533d56d0da
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-08-17 03:33:37 -0600

    [UPDATED] added new dsign for emails

--- resources/views/emails/recovery.blade.php
+++ resources/views/emails/recovery.blade.php
@@ -2,9 +2,9 @@
   <head>
   </head>
   <body style="margin: 0px;">
-    <div style="margin: auto;width: 100vw;height: auto;display: flex;background-image: url(http://test.codeguate.com/gallo/fondo-canciones.png);background-size: auto 100%;background-repeat: no-repeat;">
+    <div style="margin: auto;width: 100vw;height: auto;display: flex;background-image: url(http://documentos.devcodegt.com/gallo/fondo-canciones.png);background-size: auto 100%;background-repeat: no-repeat;">
       <div style="width: 100vw;height: 100vh;text-align: center;background-color: #922c2a99;margin-top: 0px;color: #ffffff;">
-        <img src="http://test.codeguate.com/gallo/logo.png" style="height: 30vh;width: auto;margin-top: 25px;">
+        <img src="http://documentos.devcodegt.com/gallo/logo.png" style="height: 30vh;width: auto;margin-top: 25px;">
         <h2 style="font-size: 4vh;">Nombre: <span id="name-email"> {{ $username }} </span></h2>
         <h3 style="font-size: 4vh;">Contrase&ntilde;a: <span id="password-email"> {{ $password }} </span></h3>
         <p style="text-align:center;width:100%;font-size: 2vh;color:#ffffff;">
