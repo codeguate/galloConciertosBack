ed77215e2f8d6721831993b1c12bbf6c18c5f42c
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-08-20 23:06:50 -0600

    [UPDATED] added new email template

--- resources/views/emails/recovery.blade.php
+++ resources/views/emails/recovery.blade.php
@@ -8,7 +8,7 @@
         <h2 style="font-size: 30px;"><span id="name-email">{{ $username }} </span> tu contraseña ha sido reestablecida con exito.</h2>
         <h3 style="font-size: 30px;">Contrase&ntilde;a: <span id="password-email"> {{ $password }} </span></h3> 
         <div style="text-align:center;width:100%;font-size: 30px;">
-              *Esta contraseña fue generada automaticamente luego de haber olvidado la contraseña ingresada, si necesita generar una nueva contraseña puede hacerlo en la opcion <a href="https://somosinflumedia.com/gallo" target="_blank">¿Olvidaste tu contraseña?</a> 
+              *Esta contraseña fue generada automaticamente, si necesita generar una nueva contraseña puede hacerlo <a href="https://somosinflumedia.com/gallo" target="_blank">AQUI</a> 
         </div>
       </div>
     </div>
