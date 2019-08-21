6e62a28f6245af3a5f30fdd89745a4fbf72e1d3c
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-08-20 23:02:44 -0600

    [UPDATED] added new email template

--- resources/views/emails/recovery.blade.php
+++ resources/views/emails/recovery.blade.php
@@ -2,14 +2,14 @@
   <head>
   </head>
   <body style="margin: 0px;">
-    <div style="margin: auto;width: 100vw;height: auto;display: flex;background-image: url(http://documentos.devcodegt.com/gallo/fondo-canciones.png);background-size: auto 100%;background-repeat: no-repeat;">
-      <div style="width: 100vw;height: 100vh;text-align: center;background-color: #922c2a99;margin-top: 0px;color: #ffffff;">
-        <img src="http://documentos.devcodegt.com/gallo/logo.png" style="height: 30vh;width: auto;margin-top: 25px;">
-        <h2 style="font-size: 4vh;">Nombre: <span id="name-email"> {{ $username }} </span></h2>
-        <h3 style="font-size: 4vh;">Contrase&ntilde;a: <span id="password-email"> {{ $password }} </span></h3>
-        <p style="text-align:center;width:100%;font-size: 2vh;color:#ffffff;">
-          *Esta contraseña fue generada automaticamente luego de haber olvidado la contraseña ingresada, si necesita generar una nueva contraseña puede hacerlo en la opcion <a href="http://independenciagallo.devcodegt.com/" target="_blank">¿Olvidaste tu contraseña?</a> 
-        </p>
+    <div style="margin: auto;width: 450px;height: 654px;display: flex;background-image: url(http://documentos.devcodegt.com/gallo/fondo-mobile.png);background-size: cover;background-repeat: no-repeat;">
+      <div style="width: 450px;height: 654px;text-align: center;background-color: #922c2a99;margin-top: 0px;color: #ffffff;">
+        <img src="http://documentos.devcodegt.com/gallo/logo.png" style="width: 300px;margin-top: 25px;margin: auto;margin-top:20px">
+        <h2 style="font-size: 30px;"><span id="name-email">{{ $username }} </span> tu contraseña ha sido reestablecida con exito.</h2>
+        <h3 style="font-size: 30px;">Contrase&ntilde;a: <span id="password-email"> {{ $password }} </span></h3> 
+        <div style="text-align:center;width:100%;font-size: 30px;">
+              *Esta contraseña fue generada automaticamente luego de haber olvidado la contraseña ingresada, si necesita generar una nueva contraseña puede hacerlo en la opcion <a href="https://somosinflumedia.com/gallo" target="_blank">¿Olvidaste tu contraseña?</a> 
+        </div>
       </div>
     </div>
   </body>
