8b19b6b7ba076fbddf123b556abb79b065a93789
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-08-27 18:05:58 -0600

    [UPDATED} added n to email

--- resources/views/emails/voto.blade.php
+++ resources/views/emails/voto.blade.php
@@ -5,7 +5,7 @@
     <div style="margin: auto;width: 450px;height: 654px;display: flex;background-image: url(http://documentos.devcodegt.com/gallo/fondo-mobile.png);background-size: cover;background-repeat: no-repeat;">
       <div style="width: 450px;height: 654px;text-align: center;margin-top: 0px;color: #ffffff;">
         <img src="http://documentos.devcodegt.com/gallo/logo.png" style="width: 300px;margin-top: 25px;margin: auto;margin-top:20px">
-        <h2 style="font-size: 30px;"><span id="name-email">{{ $username }} </span> bienvenido al festival de independencia.</h2>
+        <h2 style="font-size: 30px;"><span id="name-email">{{ $username }} </span> bienvenido al festival de independecia.</h2>
         <!-- <h3 style="font-size: 4vh;">Contrase&ntilde;a: <span id="password-email">*****</span></h3> -->
         <div style="text-align:center;width:100%;">
           <a href="https://somosinflumedia.com/gallo">
